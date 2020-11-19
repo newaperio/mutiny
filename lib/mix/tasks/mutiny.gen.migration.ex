9b8e547d453339f4854c6c55ee211dc2f0b4a478
Nicholas Scheurich <nick@scheurich.me>
2020-11-19 15:58:54 -0600

    Add docs for Mix task

--- lib/mix/tasks/mutiny.gen.migration.ex
+++ lib/mix/tasks/mutiny.gen.migration.ex
@@ -1,6 +1,12 @@
 defmodule Mix.Tasks.Mutiny.Gen.Migration do
   use Mix.Task
 
+  @shortdoc "Generates the Mutiny migration file"
+
+  @doc """
+  Generates an Ecto migration that can be run in order to create the database
+  function that Mutiny calls.
+  """
   @impl true
   def run(_args) do
     filename = Mix.Task.run("ecto.gen.migration", ["set_up_mutiny"])
